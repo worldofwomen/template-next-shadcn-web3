9905bb2184af52ed5ff4f927dd0559fbf97b9f4a
Horus <thomas.b@worldofwomen.art>
2024-08-02 18:39:35 -0300

    chore: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
-import "./globals.css";
+import "@/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/utils/cn";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans"
+});
+
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
